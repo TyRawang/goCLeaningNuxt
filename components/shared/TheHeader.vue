d4e312b48405627f5c3d518c25c2eb442652b676
arifhossain-saeed <arifhossain.saeed@hotmail.com>
2020-08-04 09:17:31 +0600

    minor cleanup

--- components/shared/TheHeader.vue
+++ components/shared/TheHeader.vue
@@ -6,10 +6,10 @@
         <a href="tel:+14037684186">(403)-768-4186</a>
       </div>
       <div>
-        <nuxt-link to="https://www.facebook.com/gocleaningcalgary/"><i class="fab fa-facebook-f fa-2x"></i></nuxt-link>
-        <nuxt-link to="https://www.instagram.com/gocleaningcalgary/"><i class="fab fa-instagram fa-2x"></i></nuxt-link>
-        <nuxt-link to="https://www.linkedin.com/company/18969594/"><i class="fab fa-linkedin-in fa-2x"></i></nuxt-link>
-        <nuxt-link to="mailto: info@gocleaning.ca"><i class="fas fa-envelope fa-2x"></i></nuxt-link>
+        <a href="https://www.facebook.com/gocleaningcalgary/"><i class="fab fa-facebook-f fa-2x"></i></a>
+        <a href="https://www.instagram.com/gocleaningcalgary/"><i class="fab fa-instagram fa-2x"></i></a>
+        <a href="https://www.linkedin.com/company/18969594/"><i class="fab fa-linkedin-in fa-2x"></i></a>
+        <a href="mailto: info@gocleaning.ca"><i class="fas fa-envelope fa-2x"></i></a>
       </div>
     </div>
 
