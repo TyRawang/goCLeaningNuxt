a6fbc8ed7b6dbc821295e96b39c07ca1f5d2ed46
TyRawang <31902431+TyRawang@users.noreply.github.com>
2020-08-12 17:22:08 -0600

    edit in index page, removed testimonial section

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <TheFirstSection/>
       <TheSecondSection/>
       <RequestCallBack/>
-      <TheTestimonialSectionOne/>
+      <!-- <TheTestimonialSectionOne/> -->
     </section>
 </template>
 <script>
@@ -12,9 +12,9 @@ import TheHomeBanner2 from "@/components/index/TheHomeBanner2";
 import TheFirstSection from "@/components/index/TheFirstSection";
 import TheSecondSection from "@/components/index/TheSecondSection";
 import RequestCallBack from "@/components/shared/RequestCallBack";
-import TheTestimonialSectionOne from "@/components/testimonial/TheTestimonialSectionOne"
+// import TheTestimonialSectionOne from "@/components/testimonial/TheTestimonialSectionOne"
 export default {
   components: {TheSecondSection, TheFirstSection, TheHomeBanner2, RequestCallBack,
-                TheTestimonialSectionOne }
+                 }
 }
 </script>
