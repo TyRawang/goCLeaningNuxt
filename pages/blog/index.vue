8ea33be40a93ffae61ef1387e40955ce02742ed6
TyRawang <31902431+TyRawang@users.noreply.github.com>
2020-12-11 04:55:10 -0700

    added og meta tags

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -24,12 +24,41 @@ export default {
   head() {
     return {
       title: 'Learn more about cleaning by reading our blogs | Blog', 
-      meta: [{
-        hid: 'description',
-        name: 'description', 
-        content: 'Read our blogs and learn more about cleaning and Go Cleaning. We will share all our secrets behind creating a safe and clean environment!'
-        // content: 'Let us be your perfect solution for you residential & commercial cleaning needs. Our pricing is Affordable, order your FREE QUOTE today!'
-      }]
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', 
+          content: 'Read our blogs and learn more about cleaning and Go Cleaning. We will share all our secrets behind creating a safe and clean environment!'
+          // content: 'Let us be your perfect solution for you residential & commercial cleaning needs. Our pricing is Affordable, order your FREE QUOTE today!'
+        }, 
+        {
+          hid: 'og:type',
+          name: 'og-type',
+          content: 'website'
+        }, 
+        {
+          hid: 'og-url',
+          name: 'og-url',
+          content: process.env.BASE_URL 
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: 'Learn more about cleaning by reading our blogs | Blog'
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: 'Read our blogs and learn more about cleaning and Go Cleaning. We will share all our secrets behind creating a safe and clean environment!'
+        }, 
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content: 'https://res.cloudinary.com/dmw96e6jq/image/upload/v1607687583/rawpixel-679094-unsplash_sfrxw0.jpg'
+        }
+        
+
+      ]
     }
   }
 }
